050cb7f888fbf78b0c814882cc521c61f864b305
Louis-Saglio <louis.saglio@sfr.fr>
2018-05-01 11:37:44 +0200

    begin to transform hackish mvp design into a well designed OO solution

--- with_gui.py
+++ with_gui.py
@@ -12,15 +12,60 @@ class Direction:
         self.height_dir = height_dir
 
 
-class Cell:
+class Value:
+    colors = {'M': 'blue', ' ': 'yellow', 'aspirateur': 'red', 'passed': 'green'}
 
-    def __init__(self, room: "RoomGui", value):
+    def __init__(self, value):
+        self.value = value
+
+    def __hash__(self):
+        return hash(self.value)
+
+
+class Cell:
+    cells = {}
+
+    def __init__(self, room: "RoomGui", value: Value, coordinates):
         self.room = room
         self.value = value
         self.contains = set()
+        self.coordinates = coordinates
+        self.passed = False
+        Cell.cells[self.coordinates] = self
+
+    def move_in(self, pawn):
+        self.contains.add(pawn)
+        if isinstance(pawn, Aspirateur):
+            self.passed = True
+
+    def move_from(self, pawn):
+        self.contains.remove(pawn)
 
     def __contains__(self, item):
         return item in self.contains
+
+    def contains_aspirateur(self):
+        for item in self.contains:
+            if isinstance(item, Aspirateur):
+                return True
+        return False
+
+    def show(self):
+        if self.contains_aspirateur():
+            color = Value.colors['aspirateur']
+        elif self.passed:
+            color = Value.colors['passed']
+        else:
+            color = Value.colors[self.value.value]
+        tk.Frame(
+            self.room,
+            height=self.room.cell_height,
+            width=self.room.cell_width,
+            background=color
+        ).grid(row=self.coordinates[0], column=self.coordinates[1])
+
+    def __hash__(self):
+        return hash(self.coordinates)
 
 
 class RoomGui(tk.Tk):
@@ -28,54 +73,27 @@ class RoomGui(tk.Tk):
     def __init__(self):
         super().__init__()
         self.title('Laspirateur')
-        self.data = room1
-        self.height = len(self.data)
-        self.width = len(self.data[0])
-        self.aspi_coord = 1, 2
-        self.passed = {self.aspi_coord}
-        self.aspi = Aspirateur()
         self.active = True
+
+        self.data = room1
+        self.cell_height = 20
+        self.cell_width = 20
+
         for i, row in enumerate(self.data):
             for j, cell in enumerate(row):
-                if cell == 'M':
-                    color = 'blue'
-                else:
-                    color = 'yellow'
-                tk.Frame(self, height=20, width=20, background=color).grid(row=i, column=j)
-        tk.Frame(self, height=20, width=20, background='red').grid(row=self.aspi_coord[0], column=self.aspi_coord[1])
+                Cell(self, Value(cell), (i, j)).show()
 
-    def move_aspi(self, x, y):
-        tk.Frame(self, height=20, width=20, background='green').grid(row=self.aspi_coord[0], column=self.aspi_coord[1])
-        self.aspi_coord = x, y
-        self.passed.add(self.aspi_coord)
-        tk.Frame(self, height=20, width=20, background='red').grid(row=self.aspi_coord[0], column=self.aspi_coord[1])
+        self.aspirateur_cell: Cell = random.choice([cell for cell in Cell.cells.values() if cell.value.value == ' '])
+        self.aspirateur_cell.move_in(Aspirateur())
+        self.aspirateur_cell.show()
 
     def mainloop(self, n=0):
         while self.active:
             self.update()
-            self.move_aspi_by_dir_code(self.aspi.random_move(self.get_aspi_adjacent()))
             time.sleep(0.05)
 
     def destroy(self):
         self.active = False
-
-    def get_aspi_adjacent(self):
-        return (
-            self.data[self.aspi_coord[0] - 1][self.aspi_coord[1]],
-            self.data[self.aspi_coord[0] + 1][self.aspi_coord[1]],
-            self.data[self.aspi_coord[0]][self.aspi_coord[1] - 1],
-            self.data[self.aspi_coord[0]][self.aspi_coord[1] + 1]
-        )
-
-    def move_aspi_by_dir_code(self, dir_code):
-        if dir_code == 0:
-            self.move_aspi(self.aspi_coord[0] - 1, self.aspi_coord[1])
-        elif dir_code == 1:
-            self.move_aspi(self.aspi_coord[0] + 1, self.aspi_coord[1])
-        elif dir_code == 2:
-            self.move_aspi(self.aspi_coord[0], self.aspi_coord[1] - 1)
-        elif dir_code == 3:
-            self.move_aspi(self.aspi_coord[0], self.aspi_coord[1] + 1)
 
 
 class Aspirateur:
