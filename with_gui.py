34a97cec1c92b574d7b08a39714359a5587c7853
Louis-Saglio <louis.saglio@sfr.fr>
2018-05-01 10:43:21 +0200

    the apirateur goes with semi-random move

--- with_gui.py
+++ with_gui.py
@@ -1,6 +1,26 @@
+import random
+import time
 import tkinter as tk
 
 from rooms import room1
+
+
+class Direction:
+
+    def __init__(self, height_dir, width_dir):
+        self.width_dir = width_dir
+        self.height_dir = height_dir
+
+
+class Cell:
+
+    def __init__(self, room: "RoomGui", value):
+        self.room = room
+        self.value = value
+        self.contains = set()
+
+    def __contains__(self, item):
+        return item in self.contains
 
 
 class RoomGui(tk.Tk):
@@ -11,7 +31,9 @@ class RoomGui(tk.Tk):
         self.data = room1
         self.height = len(self.data)
         self.width = len(self.data[0])
-        self.aspi = 1, 2
+        self.aspi_coord = 1, 2
+        self.passed = {self.aspi_coord}
+        self.aspi = Aspirateur()
         self.active = True
         for i, row in enumerate(self.data):
             for j, cell in enumerate(row):
@@ -20,19 +42,53 @@ class RoomGui(tk.Tk):
                 else:
                     color = 'yellow'
                 tk.Frame(self, height=20, width=20, background=color).grid(row=i, column=j)
-        tk.Frame(self, height=20, width=20, background='red').grid(row=self.aspi[0], column=self.aspi[1])
+        tk.Frame(self, height=20, width=20, background='red').grid(row=self.aspi_coord[0], column=self.aspi_coord[1])
 
     def move_aspi(self, x, y):
-        tk.Frame(self, height=20, width=20, background='yellow').grid(row=self.aspi[0], column=self.aspi[1])
-        self.aspi = x, y
-        tk.Frame(self, height=20, width=20, background='red').grid(row=self.aspi[0], column=self.aspi[1])
+        tk.Frame(self, height=20, width=20, background='green').grid(row=self.aspi_coord[0], column=self.aspi_coord[1])
+        self.aspi_coord = x, y
+        self.passed.add(self.aspi_coord)
+        tk.Frame(self, height=20, width=20, background='red').grid(row=self.aspi_coord[0], column=self.aspi_coord[1])
 
     def mainloop(self, n=0):
         while self.active:
             self.update()
+            self.move_aspi_by_dir_code(self.aspi.random_move(self.get_aspi_adjacent()))
+            time.sleep(0.05)
 
     def destroy(self):
         self.active = False
+
+    def get_aspi_adjacent(self):
+        return (
+            self.data[self.aspi_coord[0] - 1][self.aspi_coord[1]],
+            self.data[self.aspi_coord[0] + 1][self.aspi_coord[1]],
+            self.data[self.aspi_coord[0]][self.aspi_coord[1] - 1],
+            self.data[self.aspi_coord[0]][self.aspi_coord[1] + 1]
+        )
+
+    def move_aspi_by_dir_code(self, dir_code):
+        if dir_code == 0:
+            self.move_aspi(self.aspi_coord[0] - 1, self.aspi_coord[1])
+        elif dir_code == 1:
+            self.move_aspi(self.aspi_coord[0] + 1, self.aspi_coord[1])
+        elif dir_code == 2:
+            self.move_aspi(self.aspi_coord[0], self.aspi_coord[1] - 1)
+        elif dir_code == 3:
+            self.move_aspi(self.aspi_coord[0], self.aspi_coord[1] + 1)
+
+
+class Aspirateur:
+
+    def __init__(self):
+        self.dir = 0
+
+    def random_move(self, adjacents):
+        for i, adjacent in enumerate(adjacents):
+            if adjacent == ' ' and i == self.dir and random.randint(0, 4) != 0:
+                return i
+        self.dir = random.choice([n for n, adjacent in enumerate(adjacents) if adjacent == ' '])
+        return self.dir
 
 
 if __name__ == '__main__':
